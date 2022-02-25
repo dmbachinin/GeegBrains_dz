fcba0a7d8eec912a23be7b24ce4636b8ca725324
Dmitry Bachinin <d79123244656@yandex.ru>
2022-02-25 14:27:57 +0300

    Выполнение 2 домашнего задания

--- bachinin_dmitry_dz2/dz2_1.py
+++
@@ -1,4 +0,0 @@
-print(type(15 * 3), id((15 * 3)))
-print(type(15 / 3), id((15 / 3)))
-print(type(15 // 2), id((15 // 2)))
-print(type(15 ** 2), id((15 ** 2)))
--- bachinin_dmitry_dz2/dz_2_4.py
+++
@@ -1,5 +0,0 @@
-start_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй',
-              'директор аэлита']
-for element in start_list:
-    str_list = element.split(" ")
-    print(f"Привет, {str_list[-1].capitalize()}!")
