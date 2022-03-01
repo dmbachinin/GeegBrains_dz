f7538d9f56ead0891543bbe03290380580e2e87b
Dmitry Bachinin <d79123244656@yandex.ru>
2022-03-01 22:44:18 +0300

    Выполнение 3 домашнего задания

---
+++ bachinin_dmitry_dz3/dz_3_4.py
@@ -0,0 +1,20 @@
+def thesaurus_adv(*name_list):
+
+    ''' Функция сортировки имен и фамилий по ключам первых букв '''
+
+    name_dict = {}
+    for name in name_list:
+        key_name = name[0]
+        key_surname = name.split(" ")[1][0]
+        if key_surname in name_dict:
+            if key_name in name_dict[key_surname]:
+                name_dict[key_surname][key_name].append(name)
+            else:
+                name_dict[key_surname][key_name] = [name]
+        else:
+            name_dict[key_surname] = {key_name: [name]}
+    for key, value in name_dict.items():
+        print("{}: {}".format(key, value))
+
+
+thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
