af8e91f639b28c4673632e463a41e7ed4e1dfe65
Dmitry Bachinin <d79123244656@yandex.ru>
2022-04-03 17:28:26 +0300

    Выполнение 7 домашнего задания

---
+++ bachinin_dmitry_dz7/dz7_3.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+address_list = [*os.walk("my_project")]
+for address in address_list:
+    if "templates" in address[0]:
+        if address[1]:
+            address_dir = os.path.join(address[0], address[1][0])
+            os.makedirs("my_project\\templates", exist_ok=True)
+            shutil.move(address_dir, "my_project\\templates")
+            shutil.rmtree(os.path.dirname(address_dir))
